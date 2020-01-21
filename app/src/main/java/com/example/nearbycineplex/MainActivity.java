166d612d0c838ff0fe78cb8494ecb652b9a1c6b1
ronikstha <erosronik123@gmail.com>
2020-01-21 22:50:13 +1100

    login,register, homepage and movie showtime page linked

--- app/src/main/java/com/example/nearbycineplex/MainActivity.java
+++ app/src/main/java/com/example/nearbycineplex/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.nearbycineplex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.nearbycineplex.activities.HomeActivity;
+import com.example.nearbycineplex.activities.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signupButton;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupButton = findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                openRegisterActivity();
+
+            }
+        });
+
+
+
+
+
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openHomeActivity();
+                
+            }
+        });
+    }
+
+    private void openHomeActivity() {
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    private void openRegisterActivity() {
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/nearbycineplex/activities/HomeActivity.java
@@ -0,0 +1,62 @@
+package com.example.nearbycineplex.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.nearbycineplex.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Button detailView1;
+    private Button detailView2;
+    private Button detailView3;
+    private Button detailView4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        detailView1 = findViewById(R.id.detailView1);
+        detailView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMovieActivity();
+            }
+        });
+
+        detailView2 = findViewById(R.id.detailView2);
+        detailView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMovieActivity();
+            }
+        });
+
+        detailView3 = findViewById(R.id.detailView3);
+        detailView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMovieActivity();
+            }
+        });
+
+        detailView4 = findViewById(R.id.detailView4);
+        detailView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMovieActivity();
+            }
+        });
+    }
+
+    private void openMovieActivity() {
+        Intent intent = new Intent(this, MovieActivity.class);
+        startActivity(intent);
+    }
+}
